3b9c9d9a3654a36491d74b61f75dd24bc390d264
Jisang Ahn <michahn@umich.edu>
2023-10-18 00:38:28 -0400

    Still slow, but a bug where the parser would re-read links was fixed

--- USENIX_scraper.py
+++ USENIX_scraper.py
@@ -1,10 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-            
-
-def scrape(URL):
+def scrape(URL, num):
     response = requests.get(URL)
 
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -12,39 +9,27 @@ def scrape(URL):
     main_div = soup.find('div', id='main')
     uls = main_div.find_all('ul', class_='publ-list')
 
+    i = 0
     works = []
     for ul in uls:
+        if i > num:
+            break
         lis = ul.find_all('li', class_='entry inproceedings')
         for li in lis:
+            if i > num:
+                break
             try:
-                row = []
                 data_div = li.find('cite', class_='data tts-content')
-                if not data_div:
-                    continue
                 # find the title
                 title_div = data_div.find('span', class_='title')
-                if title_div:
-                    row.append(title_div.text)
-                publ = li.find('nav', class_='publ')
-                if not publ:
-                    continue
-                ul = publ.find('ul')
-                if not ul:
-                    continue
-                drop_down = ul.find('li')
-                if not drop_down:
-                    continue
-                inner = drop_down.find('div', class_='head')
-                if not inner:
-                    continue
-                link = inner.find('a')
-                if not link:
-                    continue
-                # print(link['href'])
-                row.append(getAbstract(link['href']))
+                # find the link to abstract
+                link = li.find('nav', class_='publ').find('ul').find('li').find('div', class_='head').find('a')
+
+                works.append([title_div.text, getAbstract(link['href'])])
+                i += 1
             except:
-                print(link)
-            works.append(row)
+                print("error at:", link)
+            
 
     return works
 
@@ -54,42 +39,16 @@ def scrape(URL):
 def getAbstract(url):
     res = requests.get(url)
     sub_soup = BeautifulSoup(res.content, 'html.parser')
-    sub_body = sub_soup.find('body')
-    if not sub_body:
-        # print('body not found')
-        return
-    outermost_div = sub_body.find('div')
-    if not outermost_div:
-        # print('outermost div not found')
-        return
-    main_div = outermost_div.find('main')
-    if not main_div:
-        # print("main not found")
-        return
-    content = main_div.find('section', id='content')
-    if not content:
-        # print('content not found')
-        return 
-    block_content = main_div.find('div', class_='block-content')
-    if not block_content:
-        # print('block content not found')
-        return 
-    outer_div = block_content.find('div', class_='field field-name-field-paper-description field-type-text-long field-label-above')
-    if not outer_div:
-        # print('outer div not found')
-        return
-    field_items = outer_div.find('div', class_='field-items')
-    if not field_items:
-        # print('field items not found')
-        return
-    p_elements = field_items.find_all('p')
+
+    p_elements = sub_soup.find('body').find('div').find('main').find('section', id='content').find('div', class_='block-content').find('div', class_='field field-name-field-paper-description field-type-text-long field-label-above').find('div', class_='field-items').find_all('p')
+
     return ' '.join([p.text for p in p_elements])
     # print(abstract)
     
 
 if __name__ == "__main__":
     # pass your url here in the argument to scrape()
-    works = scrape('https://dblp.org/db/conf/uss/uss2023.html')
+    works = scrape('https://dblp.org/db/conf/uss/uss2023.html', 423)
     for work in works:
         print("Title:", work[0])
         print("Abstract:", work[1])
